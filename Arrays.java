f8cfe88a86ab07195e0c7e8278f1be446d909906
Ikram neyse <ikram1981@gmail.com>
2020-10-28 13:04:27 -0500

    Arr

---
+++ Arrays.java
@@ -0,0 +1,25 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a number for elements of the list array");
+		int el=scan.nextInt();
+		List <Integer> list = new ArrayList<>();
+		
+		for(int i=0; i<el;i++) {
+			System.out.println("Enter Elements for the Array");
+			list.add(scan.nextInt());
+			
+		}
+		System.out.println(list);
+
+	}
+
+}
